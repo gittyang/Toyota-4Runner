063386ceb502eb85a6feca483aea3ca7b993aa8c
Tou Toua Yang <ttyang251@gmail.com>
2020-02-20 12:51:24 -0600

    Fifth commit - slides animation

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 console.log('hi');
 
 window.sr = ScrollReveal();
-sr.reveal('.faded', {
+sr.reveal('.scrollBfade', {
   duration: 1500,
   origin: 'bottom',
   distance: '300px',
   viewFactor: 0.3
 });
+
+sr.reveal('.scrollLfade', {
+  duration: 1500,
+  origin: 'left',
+  distance: '300px',
+  viewFactor: 0.3
+});
+
+sr.reveal('.scrollRfade', {
+  duration: 1500,
+  origin: 'right',
+  distance: '300px',
+  viewFactor: 0.3
+});
+
+
+
